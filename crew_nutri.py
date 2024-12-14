f78ad7f06d43c4cb5431a06fe6e6eb9c35297ee8
Silvio Lima <silviolima07@gmail.com>
2024-12-14 12:03:30 -0300

    Update crew_nutri.py

--- crew_nutri.py
+++ crew_nutri.py
@@ -17,7 +17,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 st.write("CrewNutri")
-st.write("GROQ_API_KEY: ",GROQ_API_KEY)
+print("GROQ_API_KEY: ",GROQ_API_KEY)
 st.write("OPENAI_API_KEY: ",OPENAI_API_KEY)
  
 #client = Groq(api_key=GROQ_API_KEY)
