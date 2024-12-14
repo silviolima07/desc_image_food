e7a5e10bf3d143bb6fe5d269ad4504c2dfe1e15b
Silvio Lima <silviolima07@gmail.com>
2024-12-14 11:46:58 -0300

    Update crew_nutri.py

--- crew_nutri.py
+++ crew_nutri.py
@@ -19,7 +19,7 @@ GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
 st.write("GROQ_API_KEY: ",GROQ_API_KEY)
  
-client = Groq(api_key=GROQ_API_KEY)
+#client = Groq(api_key=GROQ_API_KEY)
 
 #gpt = MyLLM.GPT4o_mini # model='gpt-4o-mini'
 llama = MyLLM.GROQ_LLAMA # model='groq/llama-3.2-3b-preview'
